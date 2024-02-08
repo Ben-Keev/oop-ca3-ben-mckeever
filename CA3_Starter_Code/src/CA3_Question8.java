924aaf916553ee9e8f212d93e903f30a3dd2f873
Ben-Keev <D00254413@student.dkit.ie>
2024-02-08 13:28:05 +0000

    Update Q8
    Added methods to get sum product difference and quotients. The order of operations is backwards from the order arguments are inputted to accomodate how numbers are placed on the stack.
    
    A switch determines what operation should be carried out when evaluating the top
    
    TODO work out the criteria for when to evaluate top. The top may be evaluated differently depending on whether there are open parentheses or multiplication symbols still present...

--- CA3_Starter_Code/src/CA3_Question8.java
+++ CA3_Starter_Code/src/CA3_Question8.java
@@ -1,9 +1,15 @@
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Stack;
+
 /**
  *  Name:
  *  Class Group:
  */
 public class CA3_Question8 {
+    
+    static Stack<Character> operators = new Stack<>();
+    static Stack<Integer> numbers = new Stack<>();
 
     /*
         Reads in an equation from the user
@@ -13,5 +19,64 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
+        evaluateEquation(equation);
+    }
+
+    // Read equations
+    public static void evaluateEquation(String equation) {
+        char[] iterate = equation.toCharArray();
+
+        for (Character c : iterate) {
+            // Check operators presence first.
+            if((operators.peek() == 'x' || operators.peek() == '/')) {
+                // The operator on top has a higher presence.
+                evaluateTop(c);
+            }
+
+            if (Character.isDigit(c)) { // Number
+                numbers.add((int) c);
+            } else{ // Operator
+                operators.add(c);
+            }
+        }
+    }
+
+    public static int evaluateTop(char operator) {
+        switch (operator) {
+            case 'x':
+                return product(numbers.pop(), numbers.pop());
+            
+            case '/':
+                return quotient(numbers.pop(), numbers.pop());
+
+            case '+':
+                return sum(numbers.pop(), numbers.pop());
+            case '-':
+                return difference(numbers.pop(), numbers.pop());
+
+            default:
+                return -1;
+        }
+    }
+
+    // The order of arguments reflects the order which elements will be popped.
+    // Function to calculate the sum of two numbers
+    public static int sum(int a, int b) {
+        return b + a;
+    }
+
+    // Function to calculate the product of two numbers
+    public static int product(int a, int b) {
+        return b * a;
+    }
+
+    // Function to calculate the difference between two numbers
+    public static int difference(int a, int b) {
+        return b - a;
+    }
+
+    // Function to calculate the quotient of two numbers
+    public static int quotient(int a, int b) {
+        return b / a;
     }
 }
